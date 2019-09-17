a7ff24cc473243216169298e88d96159061441fa
Andres Aguilar <andres.aguilar@nfl.com>
2019-09-17 11:41:01 -0700

    custom middleware for promise handler

---
+++ testing/src/middlewares/async.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default ({ dispatch }) => next => action => {
+  //Check to see if actions have a promise in the 'payload' property
+  if (!action.payload || !action.payload.then) {
+    next(action);
+    return;
+  }
+  // wait for promise to resolve and then create a new action to
+  // be dispatched
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
